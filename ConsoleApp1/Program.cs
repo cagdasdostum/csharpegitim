4813700e3cb61e5fd988bb38c4138e24c83cd961
cagdasdostum <cagdascalayir@outlook.com.tr>
2025-01-29 11:35:27 +0300

    dizilere çıtırdan giriş

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// diziler
+
+char[] array = {'a','r','r','a','y'};
+
+Console.WriteLine(array.Length);
+array.SetValue('d',1);  // 1.indexteki 'r' değerini 'd' ile değiştirdik.
+Console.WriteLine(array.GetValue(1));
+
+string[] sehirler = new string[4];
+sehirler[0] = "izmir";
+sehirler[1] = "manisa";
+sehirler[2] = "denizli";
+sehirler[3] = "burdur";
+
+for(int i = 0; i<sehirler.Length; i++)
+{
+  Console.WriteLine(sehirler[i]);
+}
+
---
+++ diziler2/Program.cs
@@ -0,0 +1,18 @@
+﻿
+int[] sayilar = {4,8,2,1,-3};
+
+Console.WriteLine($"sayı adedi: {sayilar.Length}");
+Console.WriteLine("Dizideki sayılar:");
+for(int i = 0; i<sayilar.Length; i++)
+{
+  Console.Write($"{sayilar[i]}, ");
+}
+
+int toplam = 0;
+
+foreach(int sayi in sayilar) 
+{
+  toplam += sayi;
+}
+
+Console.WriteLine( $"\ntoplam: {toplam}");
