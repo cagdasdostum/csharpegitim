36133595b7801b98a07bac5d6e1ec16060a76de4
cagdasdostum <cagdascalayir@outlook.com.tr>
2025-01-29 13:59:08 +0300

    dizilerden

---
+++ altdizi/Program.cs
@@ -0,0 +1,11 @@
+﻿int[] sayilar = {5,1,65,7,8,9,0};
+
+Console.WriteLine(sayilar[2]);
+Console.WriteLine(sayilar[^2]);
+Console.WriteLine(sayilar[2..5]); //ekrana istediğimiz değeri yazdırmaz veri tipini yazdırır. foreach kullanarak yazılabilir.
+
+var yeniDizi2 = sayilar[^7..];
+foreach(var s in yeniDizi2)
+{
+  Console.Write(s + " ");
+}
