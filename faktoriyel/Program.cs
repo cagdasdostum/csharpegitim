8dfc52bb683561400819ea97008e68116fa7bb2f
cagdasdostum <cagdascalayir@outlook.com.tr>
2025-01-29 11:08:19 +0300

    faktöriyel hesaplayan program

--- faktoriyel/Program.cs
+++ faktoriyel/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("bir sayı girin:");
+int sayi = Convert.ToInt32(Console.ReadLine());
+
+if(sayi>0)
+{
+  int sonuc = 1;
+  for(int i = sayi; i >=1; i--)
+  {
+    sonuc*=i;
+  }
+
+  Console.WriteLine($"{sayi}! = {sonuc}");
+}
+else
+{
+  Console.WriteLine("negatif sayıların faktöriyeli hesaplanamaz.");
+}
