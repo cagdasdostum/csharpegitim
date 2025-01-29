f17e6ad9f2cefed1afe5e6ba2bc9ff7b60296b72
cagdasdostum <cagdascalayir@outlook.com.tr>
2025-01-29 16:34:30 +0300

    queue ve stack kavramları

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,28 @@
+﻿Queue<int> kuyruk = new();
+
+//eklemek
+kuyruk.Enqueue(4); 
+kuyruk.Enqueue(3);
+kuyruk.Enqueue(6);
+kuyruk.Enqueue(9);
+
+while(kuyruk.Count>0)
+{
+  int eleman = kuyruk.Dequeue();  //kuyruktan çıkıyor
+  Console.Write($"{eleman}, ");
+}
+
+Console.WriteLine("");
+
+Stack<int> yigin = new();
+
+yigin.Push(4);
+yigin.Push(3);
+yigin.Push(6);
+yigin.Push(9);
+
+while(yigin.Count>0)
+{
+  int eleman = yigin.Pop();
+  Console.Write($"{eleman}, ");
+}
