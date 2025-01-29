7d1b6867179f6ddc022808a835d4db99f5cea8d0
cagdasdostum <cagdascalayir@outlook.com.tr>
2025-01-29 16:20:56 +0300

    dictionary örnek

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,39 @@
+﻿
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Dictionary<int, string> ogrenciler = new(); //c# yeni versiyonlarındda böyle kullanılabiliyormuş.
+        int numara = 0;
+        string ad = "";
+        do
+        {
+            Console.WriteLine("Eklenecek öğrencinin numarasını girin:");
+            numara = Convert.ToInt32(Console.ReadLine());
+            if (numara != 0)
+            {
+
+              if(ogrenciler.ContainsKey(numara))
+              {
+                Console.WriteLine($"{numara} numaralı öğrenci zaten var.");
+                continue;
+              } 
+                
+
+                Console.WriteLine("Eklenecek öğrencinin adını girin:");
+                ad = Console.ReadLine();
+
+                ogrenciler.Add(numara, ad); //yeni öğrenciyi listeye ekle.
+            }
+
+
+
+        } while (numara != 0);
+
+        Console.WriteLine("Öğrenci No | Öğrenci Adı");
+        foreach(var ogr in ogrenciler)
+        {
+          Console.WriteLine($"{ogr}");
+        }
+    }
+}
