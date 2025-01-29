e878a1c89f1849dc89232a06fdd4d5c1ef25da45
cagdasdostum <cagdascalayir@outlook.com.tr>
2025-01-29 16:52:27 +0300

    girilen kelimeyi tersten yazdıran program

---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,23 @@
+﻿//  klavyeden girilen bir metni stack kullanarak tersinden yazdırın.
+//  örnek: programlama => amalmargorp
+
+
+
+Stack<char> yigin = new();
+string metin;
+
+Console.WriteLine("metin girin:");
+metin = Console.ReadLine();
+
+
+
+
+foreach(char c in metin)
+{
+  yigin.Push(c);
+}
+
+while(yigin.Count>0) {
+  char c = yigin.Pop();
+  Console.Write($"{c}");
+}
