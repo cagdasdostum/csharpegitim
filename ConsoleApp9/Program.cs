c7d61eb78d7afce8b5689c9f5446c5e8673db08a
cagdasdostum <cagdascalayir@outlook.com.tr>
2025-01-30 11:34:56 +0300

    sınıflar örnek

---
+++ ConsoleApp9/Program.cs
@@ -0,0 +1,44 @@
+﻿List<Ogrenci> liste = new();
+
+
+do
+{
+    Console.WriteLine("Eklenecek numara:");
+    int num = Convert.ToInt32(Console.ReadLine());
+    
+    if(num==0)  break;
+
+    Console.WriteLine("İsmini girin:");
+    string? isim = Console.ReadLine();
+    Console.WriteLine("Ortalamasını girin:");
+    double ort = Convert.ToDouble(Console.ReadLine());
+
+    Ogrenci ogr = new();
+    ogr.numara = num;
+    ogr.isim = isim;
+    ogr.ortalama = ort;
+
+    liste.Add(ogr);
+
+
+} while (true);
+
+foreach (Ogrenci o in liste)
+{
+    Console.WriteLine($"no:{o.numara}, isim:{o.isim}, ortalama:{o.ortalama}");
+}
+
+
+
+
+
+class Ogrenci
+{
+    public int numara;
+    public string? isim;
+    public double ortalama;
+
+}
+
+
+
