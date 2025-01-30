cbb755d8ed9aeea8bd3d526bc0f7dde2621f4ce1
cagdasdostum <cagdascalayir@outlook.com.tr>
2025-01-30 13:48:58 +0300

    methodslar giriş

---
+++ ConsoleApp11/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace ConsoleApp11;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int toplamDeger = Topla(34,56);
+        Console.WriteLine(toplamDeger);
+        EkraniTemizle();
+        Console.WriteLine(Topla(5,14));
+        Bekle();
+
+
+    
+    }
+
+    static void EkraniTemizle()
+    {
+        Console.Clear();
+    }
+
+    static void Bekle()
+    {
+        Console.WriteLine("Çıkmak için bir tuşa basın");
+        Console.ReadKey();
+    }
+
+    public static int Topla(int a, int b)
+    {
+        int sonuc = a+b;
+        return sonuc;                
+    }
+}
