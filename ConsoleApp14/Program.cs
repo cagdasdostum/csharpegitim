d144e6a20d51f188ccdfb8a4bfefb442ccd9350c
cagdasdostum <cagdascalayir@outlook.com.tr>
2025-01-30 15:40:51 +0300

    parametreli metotlarla alakalı bir örnek.

---
+++ ConsoleApp14/Program.cs
@@ -0,0 +1,27 @@
+﻿//  varsayılan parametreli metot
+double UcretHesapla(double fiyat, double miktar=1, double indirim=0)
+{
+  double tutar = fiyat*miktar;
+  if (indirim>0)
+  {
+    tutar = tutar - tutar*(indirim/100);
+  }
+
+  return tutar;
+}
+
+
+//  değişken parametreli metot
+void AltAltaYaz(params string[] liste)
+{
+  foreach (string str in liste)
+  {
+    Console.WriteLine(str);
+  }
+}
+
+Console.WriteLine($"Ücret:{UcretHesapla(16)}");
+Console.WriteLine($"Ücret:{UcretHesapla(16, 20)}");
+Console.WriteLine($"Ücret:{UcretHesapla(16, 20, 30)}");
+
+AltAltaYaz("İzmir", "Manisa", "Burdur", "Denizli");
