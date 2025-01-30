dfa6cf7b0d09f9909f050bab698ed3a393021929
cagdasdostum <cagdascalayir@outlook.com.tr>
2025-01-30 15:13:49 +0300

    kendimize özel konsol fonskiyonları yazıp kullandık.

---
+++ ConsoleApp13/Program.cs
@@ -0,0 +1,9 @@
+﻿Konsol kon = new();
+
+
+kon.Temizle(ConsoleColor.Red);
+
+int s1 = kon.SayiOku("Bir sayı girin:");
+int s2 = kon.SayiOku("Başka bir sayı girin:");
+
+kon.YazSatir($"Toplam = {s1 + s2}");
