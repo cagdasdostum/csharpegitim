974c9ee58032d445b8d4fdc0d60a057fccc16774
cagdasdostum <cagdascalayir@outlook.com.tr>
2025-01-31 12:01:26 +0300

    c diskinde yeni klasör oluşturduk. c diskindeki klasörleri ve dosyaları listeledik.

---
+++ ConsoleApp18/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+
+string path = @"C:\BtkCSharp";
+string cDiski = @"C:\\";
+Directory.CreateDirectory(path);
+Console.WriteLine($"{path} isimli klasör oluşturuldu.");
+
+if (Directory.Exists(path))
+{
+  Console.WriteLine("Klasör bu dizinde zaten var.");
+}
+
+string[] altKlasorler = Directory.GetDirectories(cDiski);
+Console.WriteLine($"Klasörler: {altKlasorler.Length}");
+foreach (string klasor in altKlasorler)
+{
+  Console.WriteLine(klasor);
+}
+
+string[] altDosyalar = Directory.GetFiles(cDiski);
+Console.WriteLine($"Dosyalar: {altDosyalar.Length}");
+
+foreach (string dosya in altDosyalar)
+{
+  Console.WriteLine(dosya);
+}
