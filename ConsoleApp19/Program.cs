524752465f9e86e23e7caa8b6c63322d72853f3f
cagdasdostum <cagdascalayir@outlook.com.tr>
2025-01-31 14:13:40 +0300

    biraz daha dosya işlemi

---
+++ ConsoleApp19/Program.cs
@@ -0,0 +1,19 @@
+﻿string? folderPath = @"C:\BtkCSharp";
+if(!Directory.Exists(folderPath))
+{
+  Directory.CreateDirectory(folderPath);
+  Console.WriteLine("Klasör oluşturuldu.");
+}
+
+string filePath = folderPath + @"\merhaba.txt";
+string icerik = "Merhaba c# programlama dili";
+
+File.WriteAllText(filePath, icerik);
+Console.WriteLine("İçerik oluştruldu.");
+
+string okunan = File.ReadAllText(filePath);
+Console.WriteLine($"Okunan veri: {okunan}");
+
+string yeniIcerik = "\nbugün günlerden Cuma";
+
+File.AppendAllText(filePath, yeniIcerik);
